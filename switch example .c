20d999451d7716a18cbb407b87458226767c144a
salominreji0621 <salominrejir@gmail.com>
2025-05-17 06:28:56 -0700

    Create switch example .c

---
+++ switch example .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    switch(31)
+    {
+        case 11+2:printf("A");
+        break;
+        defalut:printf("vet mess");
+        break;
+        case 2+1:printf("B");
+    }
+    return 0;
+}
