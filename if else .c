1aba0a66c32ba22912731f3afce5ec6d7fff2588
salominreji0621 <salominrejir@gmail.com>
2025-05-17 06:22:48 -0700

    Create if else .c

---
+++ if else .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf("a");
+    printf("b");
+    if(3)
+    {
+        printf("C");
+        printf("d");
+    }
+    printf("e");
+    printf("f");
+    return 0;
+}
